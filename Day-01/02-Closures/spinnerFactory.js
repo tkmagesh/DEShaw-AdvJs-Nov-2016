abdd9b8a5e2f4f80b932fe2f8bca765bb79feac9
Magesh <tkmagesh77@gmail.com>
2016-11-24 17:08:38 +0530

    Day-01 Complete

---
+++ Day-01/02-Closures/spinnerFactory.js
@@ -0,0 +1,18 @@
+
+function spinnerFactory(){
+	var counter = 0;
+
+
+	function up(){
+		return ++counter;
+	}
+
+	function down(){
+		return --counter;
+	}
+
+	return {
+		up : up,
+		down : down
+	}
+}
